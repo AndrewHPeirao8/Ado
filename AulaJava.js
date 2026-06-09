69540dce842c00b05a6669e7be6b09476c9be50d
Andrew Peirao <andrew63512336@alunos.sc.senac.br>
2026-06-08 21:29:49 -0300

    vem sem do

---
+++ AulaJava.js
@@ -0,0 +1,15 @@
+let nome = "Vanessa";
+let idade = 21;
+let ativo = true;
+
+let pessoa = 
+{
+    nome : "Vanelssa",
+    idade : 21,
+    ativo : true
+};
+
+let x = 5;
+let y = "5";
+
+console.log
