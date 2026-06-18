3bf5902139ff164164a941c9f12fa73421d96067
Andrew Peirao <andrew63512336@alunos.sc.senac.br>
2026-06-18 20:59:25 -0300

    agr vai

---
+++ AulaC#/AulaPoo.cs
@@ -0,0 +1,10 @@
+public class Carro
+{
+    public int litragemMotor {get; set;}
+    public string marca {get; set;}
+    public string cor {get; set;}
+
+    public void Estilo()
+    {
+        
+    }
